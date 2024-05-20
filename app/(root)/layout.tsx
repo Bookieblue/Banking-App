8b46964633b6c4274a52ec50bf387a7316d52d77
Bookieblue <oluwabukola.fasan@gmail.com>
2024-05-20 09:25:43 +0100

    sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
-import type { Metadata } from "next";
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 
 
@@ -7,10 +9,19 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+     const loggedIn = { firstName: 'Bukola', lastname: 'Fasan'};
   return (
-    <main>
-        SIDEBAR
-       {children} 
+    <main className="flex h-screen w-full font-inter">
+       <Sidebar user={loggedIn} />
+       <div className="flex size-full flex-col">
+             <div className="root-layout">
+                <Image src='/icons/logo.svg' width={30} height={30} alt='menu icon' />
+             </div>
+             <div>
+             <MobileNav user={loggedIn} />
+             </div>
+             {children}
+       </div> 
     </main>
   );
 }
